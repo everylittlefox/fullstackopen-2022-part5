39b02f4704c45f748e13012c3aebb00e2872154c
Favour Anefu <favouranefu@gmail.com>
2022-11-28 15:10:25 +0100

    completed 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
 test('shows only title and author by default', () => {
   const blog = {
@@ -16,10 +17,32 @@ test('shows only title and author by default', () => {
   const blogSummary = container.querySelector('.blog-summary')
   expect(blogSummary).toBeDefined()
 
-  const texts = [...blogSummary.childNodes].map(c => c.textContent)
+  const texts = [...blogSummary.childNodes].map((c) => c.textContent)
   expect(texts).toContain('Michael Chan')
   expect(texts).toContain('React patterns')
 
   const blogDetails = container.querySelector('.blog-expanded-details')
   expect(blogDetails).toBeNull()
+})
+
+test('shows details when view button is clicked', async () => {
+  const blog = {
+    id: 1,
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    user: { name: 'root', username: 'root', id: 1 }
+  }
+
+  const { container } = render(<Blog blog={blog} />)
+  const viewButton = screen.getByText('view')
+
+  await userEvent.click(viewButton)
+
+  const blogDetails = container.querySelector('.blog-expanded-details')
+  expect(blogDetails).not.toBeNull()
+
+  const texts = [...blogDetails.childNodes].map(c => c.textContent)
+  expect(texts).toContain(blog.url)
 })
