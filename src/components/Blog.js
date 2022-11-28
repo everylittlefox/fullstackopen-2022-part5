90d3db2a10ca41b6fa9296a94ec77db26b59c167
Favour Anefu <favouranefu@gmail.com>
2022-11-28 11:57:22 +0100

    completed 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,35 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5
+}
+
+const Blog = ({ blog: { title, author, url, likes, user } }) => {
+  const [expanded, setExpanded] = useState(false)
+  const buttonLabel = expanded ? 'hide' : 'view'
+
+  const toggleExpanded = () => setExpanded(!expanded)
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {title} {author} <button onClick={toggleExpanded}>{buttonLabel}</button>
+      </div>
+      {expanded && (
+        <div>
+          <p>{url}</p>
+          <p>
+            likes {likes} <button>like</button>
+          </p>
+          <p>{user.name}</p>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
